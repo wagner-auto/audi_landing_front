3094e9b7659cc2185a9bd0107bc26519b443a33f
deli13 <death13life@gmail.com>
2019-04-23 13:50:58 +0300

    stock

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,17 +1,38 @@
 import "owl.carousel";
-var owl=$(".carousel");
+var car_items = $(".special_block__carousel");
+var owl = $(".carousel");
+/**
+ * Инициализация карусели
+ */
+car_items.on("initialized.owl.carousel", function (e) {
+    $(".special_block__carousel-counter .owl-counter").html("1 / <span>" + e.item.count+"</span>");
+});
 owl.owlCarousel({
-    loop:true,
-    thumbs:false,
+    loop: true,
+    thumbs: false,
     // nav:true,
     // autoWidth:true,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    items:1,
-    lazyLoad:true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    items: 1,
+    lazyLoad: true,
     itemsDesktop: ["100%", 1],
     itemsDesktopSmall: ["100%", 1],
     itemsMobile: ["100%", 1],
     itemsTablet: ["100%", 1]
+});
+
+/**
+ * Смена слайда
+ */
+car_items.on("changed.owl.carousel", function (e) {
+    $(".special_block__carousel-counter .owl-counter").html(++e.page.index + " / <span>" + e.page.count+"</span>");
+});
+$(".icon-counter").on("click",function (e) {
+    if($(this).hasClass("owl-next")){
+        car_items.trigger("next.owl.carousel");
+    } else {
+        car_items.trigger("prev.owl.carousel");
+    }
 });
